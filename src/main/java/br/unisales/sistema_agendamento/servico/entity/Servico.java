a1455faa8f6816031aea15f2fe19ce197cc8eb67
Mateus Alves Costa <mateuscosta3497@gmail.com>
2026-09-07 15:25:26 -0300

    feat: torna o campo nome unico em Servico

--- src/main/java/br/unisales/sistema_agendamento/servico/entity/Servico.java
+++ src/main/java/br/unisales/sistema_agendamento/servico/entity/Servico.java
@@ -30,7 +30,7 @@ public class Servico {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, unique = true)
     private String nome;
 
     @Column(nullable = false, length = 255)
