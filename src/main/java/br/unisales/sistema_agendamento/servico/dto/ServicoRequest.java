adf9b91e7e61fe4bf22fa8a27bcdd61c0634976e
Mateus Alves Costa <mateuscosta3497@gmail.com>
2026-09-07 23:40:29 -0300

    feat: adiciona valicaçao no atributo ativo

--- src/main/java/br/unisales/sistema_agendamento/servico/dto/ServicoRequest.java
+++ src/main/java/br/unisales/sistema_agendamento/servico/dto/ServicoRequest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 
 public record ServicoRequest(
@@ -13,10 +14,13 @@ public record ServicoRequest(
     @NotBlank(message = "A descrição do serviço não pode ser nula ou vazia.")
     String descricao,
 
-    @NotBlank(message = "A duração do serviço não pode ser nula.")
+    @NotNull(message = "A duração do serviço não pode ser nula.")
     @PositiveOrZero(message = "A duração do serviço não pode ser negativa.")
     Integer duracaoMinutos,
 
     @DecimalMax(value = "1000.00", message = "O preço do serviço não pode ser maior que 1000.00.")
-    BigDecimal preco
+    BigDecimal preco,
+
+    @NotNull(message = "O status do serviço não pode ser nulo.")
+    Boolean ativo
 ) {}
