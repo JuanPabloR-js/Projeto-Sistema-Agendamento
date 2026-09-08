4189e815dfa140318c2111bd9a99dda5d746339c
breno <brenin401@gmail.com>
2026-09-08 17:14:39 -0300

    feat: implementar UsuarioRepository

---
+++ src/main/java/br/unisales/sistema_agendamento/usuario/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.unisales.sistema_agendamento.usuario.repository;
+
+import br.unisales.sistema_agendamento.usuario.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+}
