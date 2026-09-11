90fe337bc0d711ca4565d310f864f4e9c8e531d6
JuanPRH_jv <jprochah@gmail.com>
2026-09-11 16:23:14 -0300

    refactor: adiciona telefone na tabela

--- src/main/resources/db/migration/V2__create_tb_usuario.sql
+++ src/main/resources/db/migration/V2__create_tb_usuario.sql
@@ -2,6 +2,7 @@ CREATE TABLE tb_usuario (
     id BIGSERIAL PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     email VARCHAR(150) NOT NULL,
+    telefone VARCHAR(20),
     senha VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
     ativo BOOLEAN NOT NULL DEFAULT TRUE,
