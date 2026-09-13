0083ed2129eea9a582069f5f8dfefe3635f25e75
Joao Marques <joaovictormmarques09@gmail.com>
2026-09-13 13:35:29 -0300

    adiciona testes e ajutes no modulo barbeiro

--- src/main/java/br/unisales/sistema_agendamento/barbeiro/dto/BarbeiroRequestDTO.java
+++ src/main/java/br/unisales/sistema_agendamento/barbeiro/dto/BarbeiroRequestDTO.java
@@ -3,35 +3,48 @@ package br.unisales.sistema_agendamento.barbeiro.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public class BarbeiroRequestDTO { // Dados entram na aplicação
+public class BarbeiroRequestDTO {
 
-    @NotNull (message = "O usuário é obrigatório.")
+    // ID do usuário que será associado ao barbeiro.
+    @NotNull(message = "O usuário é obrigatório.")
     private Long usuarioId;
 
-    @NotBlank (message = "A especialidade é obrigatória.")
-    private String especialidade; // Serve para não deixar o campo de especialidade vazio
+    // Especialidade do barbeiro.
+    @NotBlank(message = "A especialidade é obrigatória.")
+    private String especialidade;
 
-    public BarbeiroRequestDTO(){
+    // Construtor vazio.
+    // É importante para frameworks como o Spring/Jackson.
+    public BarbeiroRequestDTO() {
     }
 
-    public BarbeiroRequestDTO (Long ususarioId, String especialidade){ // construtor
+    // Construtor usado para criar o DTO com os dados informados.
+    public BarbeiroRequestDTO(
+            Long usuarioId,
+            String especialidade) {
+
         this.usuarioId = usuarioId;
         this.especialidade = especialidade;
     }
 
-    public Long getUsuarioId(){ // Getters e Setters
+    // Retorna o ID do usuário.
+    public Long getUsuarioId() {
         return usuarioId;
     }
 
-    public void setUsuarioId (Long usuarioID){
+    // Altera o ID do usuário.
+    public void setUsuarioId(Long usuarioId) {
         this.usuarioId = usuarioId;
     }
 
-    public String getEspecialidade(){
+    // Retorna a especialidade.
+    public String getEspecialidade() {
         return especialidade;
     }
 
-    public void setEspecialidade (String especialidade){
+    // Altera a especialidade.
+    public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
     }
 }
+
